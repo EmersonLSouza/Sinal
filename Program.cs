1ba6b06159ceb7da0369806e666c551a4c3f126c
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 19:45:03 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.WriteLine("Voce digitou um numero positivo!");
+}
+
+else if (numero < 0)
+{
+    Console.WriteLine("Voce digitou um número negativo!");
+}
+else
+{
+    Console.WriteLine("Voce digitou o número zero!");
+}
